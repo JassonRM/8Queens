280771e64ef248aed38a0247df70f5a8d723416f
Róger Valderrama <roger.andres18@gmail.com>
2018-05-08 23:51:28 -0600

    Start Button Added, FIXED image crash

--- app/src/main/java/jassonrm/a8queens/Board.java
+++ app/src/main/java/jassonrm/a8queens/Board.java
@@ -2,12 +2,25 @@ package jassonrm.a8queens;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Board extends AppCompatActivity {
+
+    private Button startBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
+        startBtn = findViewById(R.id.startBtn);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("CLICKED");
+            }
+        });
+
     }
 }
